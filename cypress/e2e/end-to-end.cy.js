a713a455e81ab6de4d36416d2c9ed04993500302
Danilo Oliveira <danilooliveira4586@gmail.com>
2026-07-04 10:46:37 -0300

    Adicionando o teste de validar cadastro e login

--- cypress/e2e/end-to-end.cy.js
+++ cypress/e2e/end-to-end.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="Cypress" />
+
+import cadastroPage from "../support/pages/cadastro-page"
+import { faker } from '@faker-js/faker';
+
+describe('Testes End To End do fluxo de cadastro e login', () => {
+  beforeEach(() => {
+    cadastroPage.visitarPaginaCadastro()
+  })
+
+  it('Deve fazer o cadastro e validar o login com o usuário cadastrado', () => {
+    let email = `teste${Date.now()}@teste.com`
+    let senha = 'user123'
+
+    cadastroPage.preencherCadastro('Danilo', email, '83988888888', senha, senha)
+    cy.url().should('include', 'dashboard')
+    cy.get('.user-actions > .btn-outline-danger').click()
+    cy.login(email, senha)
+    cy.url().should('include', 'dashboard')
   })
 })
---
+++ cypress/support/pages/cadastro-page.js
@@ -0,0 +1,24 @@
+class CadastroPage {
+    visitarPaginaCadastro() {
+        cy.visit('register.html')
+    }
+
+    campoNome() {return cy.get('#name')}
+    campoEmail() {return cy.get('#email')}
+    campoTelefone() {return cy.get('#phone')}
+    campoSenha() {return cy.get('#password')}
+    campoConfirmarSenha() {return cy.get('#confirm-password')}
+    checkTermos() {return cy.get('#terms-agreement')}
+    botaoCadastrar() {return cy.get('#register-btn')}
+
+    preencherCadastro(nome, email, telefone, senha, confirmarSenha) {
+        this.campoNome().clear().type(nome)
+        this.campoEmail().clear().type(email)
+        this.campoTelefone().clear().type(telefone)
+        this.campoSenha().clear().type(senha)
+        this.campoConfirmarSenha().clear().type(confirmarSenha)
+        this.checkTermos().check()
+        this.botaoCadastrar().click()
+    }
+}
+export default new CadastroPage()
